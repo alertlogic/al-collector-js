c6ffa7f7cbb443245d9dff8a23e90debc765ffa5
tomdos <chimpy@seznam.cz>
2018-01-30 16:42:24 +0000

    ingest supports vpcflow messages
    vpcflow msg

--- test/al_mock.js
+++ test/al_mock.js
@@ -1,14 +1,14 @@
 
-const AIMS_TEST_CREDS = {
+const AIMS_CREDS = {
     access_key_id: 'test-access-key-id',
     secret_key: 'test-secret-key'
 };
 
-const TEST_AL_API = 'api.test.product.dev.alertlogic.com';
-const INGEST_TEST_URL = '/data/secmsgs';
+const AL_API = 'al-api-endpoint.alertlogic.com';
+const INGEST_API = 'ingest-api-endpoint.alertlogic.com';
 
 module.exports = {
-    TEST_AL_API : TEST_AL_API,
-    AIMS_TEST_CREDS : AIMS_TEST_CREDS,
-    INGEST_TEST_URL : INGEST_TEST_URL
+    AIMS_CREDS : AIMS_CREDS,
+    AL_API : AL_API,
+    INGEST_API : INGEST_API
 };
