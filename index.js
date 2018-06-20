de055ee23e28c4967aaf43cb03b81263bb17e5db
Alexander Turkin <aturkin@alertlogic.com>
2018-06-20 12:29:31 +0100

    add .AlServiceC to index

---
+++ index.js
@@ -0,0 +1,14 @@
+/* -----------------------------------------------------------------------------
+ * @copyright (C) 2018, Alert Logic, Inc
+ * @doc
+ *
+ * Classes for communication to Alert Logic services.
+ *
+ * @end
+ * -----------------------------------------------------------------------------
+ */
+
+module.exports = {
+    AlServiceC : require('./al_servicec')
+};
+
