653e4085cb892246bac5b354c686a057f120bb97
Konstantin Kuzmin <kkuzmin@alertlogic.com>
2020-03-10 17:26:36 +0000

    Add agent status datatype into Ingestc.

--- test/ingestc_test.js
+++ test/ingestc_test.js
@@ -11,7 +11,7 @@ const fs = require('fs');
 const assert = require('assert');
 const sinon = require('sinon');
 const AimsC = require('../al_servicec').AimsC;
-const IngestC = require('../al_servicec').IngestC;
+const IngestC = require('../ingestc').IngestC;
 const m_alMock = require('./al_mock');
 var m_servicec = require('../al_servicec');
 
@@ -91,6 +91,20 @@ describe('Unit Tests', function() {
             var aimsc = new AimsC(m_alMock.AL_API, m_alMock.AIMS_CREDS);
             var ingest = new IngestC(m_alMock.INGEST_ENDPOINT, aimsc);
             ingest.sendLogmsgs('testing payload');
+        });
+        
+        it('Verify agentstatus body', function(done) {
+            fakePost = sinon.stub(m_servicec.AlServiceC.prototype, 'post').callsFake(
+                function fakeFn(path, extraOptions) {
+                    assert.equal(extraOptions.headers['Content-Type'], 'alertlogic.com/json');
+                    assert.equal(path, '/data/agentstatus');
+
+                    done();
+                });
+
+            var aimsc = new AimsC(m_alMock.AL_API, m_alMock.AIMS_CREDS);
+            var ingest = new IngestC(m_alMock.INGEST_ENDPOINT, aimsc);
+            ingest.sendAgentstatus('testing payload');
         });
 
     });
